81325f42a46e9524454b8ce004d6101b6e28234f
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2024-01-16 20:16:03 +0700

    update navbar, adding theme switch

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,18 +1,60 @@
-import Image from "next/image";
-const Navbar = () =>{
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import lightLogo from '../../../../public/logo-putih.svg';
+import darkLogo from '../../../../public/logo-hitam.svg';
+
+const Navbar = () => {
+  const isClient = typeof window !== 'undefined';
+  const initialTheme = isClient ? localStorage.getItem('theme') || 'light' : 'light';
+  const [theme, setTheme] = useState(initialTheme);
+  const isLightTheme = theme === 'light';
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuToggle = () => {
+      setMenuOpen(!menuOpen)
+  }  ;
+  const handleToggle = (e: { target: { checked: any; }; }) => {
+      if(e.target.checked) {
+          setTheme('dark');
+      } else {
+          setTheme('light');
+      }
+  }
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem('theme', theme);
+      const localTheme = localStorage.getItem('theme') || 'light';
+      document.querySelector('html')?.setAttribute('data-theme', localTheme);
+    }
+  }, [theme, isClient]);
+
     return (
     <div className="navbar bg-base-100 md:px-10 drop-shadow-lg">
-        <div className="navbar-start">
+        <div className="navbar-start gap-4">
             <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div tabIndex={0} className="lg:hidden">
+                <label className="btn btn-circle swap swap-rotate">
+                    <input type="checkbox" checked={menuOpen} onChange={menuToggle} />
+                    <input type="checkbox" />
+                    <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+                    <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+                </label>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Profile</a></li>
-                <li><a>Portofolio</a></li>
+            <ul tabIndex={0} className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${menuOpen ? 'block' : 'hidden'}`}>
+                <li>
+                    <a className='text-xl font-medium'>Profile</a>
+                </li>
+                <li>
+                    <a className='text-xl font-medium'>Portofolio</a>
+                </li>
             </ul>
             </div>
-                <Image src="/logo.svg" alt="" width={50} height={50} />
+                <Image 
+                    src={isLightTheme ? darkLogo : lightLogo}
+                    alt="Autofat"
+                    width={50}
+                    height={50}
+                />
             </div>
         <div className="navbar-center hidden lg:flex">
             <h1 className="font-bold text-xl">Weather App</h1>
@@ -24,7 +66,7 @@ const Navbar = () =>{
             </ul>
             <label className="swap swap-rotate">
                 {/* this hidden checkbox controls the state */}
-            <input type="checkbox" />
+            <input type="checkbox" onChange={handleToggle} />
             {/* sun icon */}
             <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
             {/* moon icon */}
