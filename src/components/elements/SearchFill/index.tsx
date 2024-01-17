5bcd244eeb2f8458008f69c2c33e5e47aa4a2442
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2024-01-17 22:17:29 +0700

    add api, fix bug

--- src/components/elements/SearchFill/index.tsx
+++ src/components/elements/SearchFill/index.tsx
@@ -1,7 +1,16 @@
-const searchFill = () =>{
-    return(    
-        <input type="text" placeholder="Location" className="input input-bordered w-xs md:w-[400px] "/>
-        
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+interface SearchFillProps {
+    handlerSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    setLocation: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const searchFill = ({handlerSearch, setLocation}: SearchFillProps) =>{
+    return( 
+        <div className="flex justify-center gap-2">
+            <input type="text" placeholder="Location" className="input input-bordered w-[300px] md:w-[400px]" onKeyDown={handlerSearch} onChange={e => setLocation(e.target.value)}/>
+        </div>   
     )
 }
 export default searchFill;
--- src/components/layouts/Content/index.tsx
+++ src/components/layouts/Content/index.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import SearchFill from "@/components/elements/SearchFill";
-import ButtonSearch from "@/components/elements/ButtonSearch";
 
 
 const Content = () => {
@@ -15,6 +14,30 @@ const Content = () => {
     }
   }, [theme, isClient]);
 
+  const[data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHERAPI_KEY}&q=${location}&days=3&aqi=yes&alerts=yes`; 
+
+  const handlerSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === "Enter"){
+        e.preventDefault()
+        try{
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error("Something went wrong");
+            }
+            const data = await response.json();
+            setData(data);
+            setLocation("Depok");
+            setError
+        }catch(error){
+            setError("Location Not Found!");
+            setData({})
+        }
+    }
+  }
+
   return (
     <main>
         <div className="flex justify-center mt-10 lg:hidden ease-in-out duration-300">
@@ -23,25 +46,31 @@ const Content = () => {
         <div className={"flex justify-center mt-10 md:mx-10 md:mt-10"}>
             <div className={`rounded-2xl bg-slate-600 h-[450px]  w-[350px] lg:w-[792px] lg:h-[650px] md:w-[650px] md:h-[538px]  ease-in-out duration-300`}>
                 <div className="flex justify-center my-6 gap-1">
-                    <SearchFill/>
-                    <ButtonSearch/>
+                    <SearchFill handlerSearch={handlerSearch} setLocation = {setLocation}/>
                 </div>
                 <div className="flex flex-col items-center h-full">
                     <h1 className="text-base-100">
-                        Wednesday, 17 January 2024
+                        Wed, 17th January 2024
                     </h1>
-                    <h1 className="text-base-100 font-bold text-6xl my-8 ">
-                        25
-                        <a className="absolute text-3xl">
-                            °C
-                        </a>
-                    </h1>
-                    <div className="flex flex-col items-center h-full gap-1">
-                        <h1 className="text-base-100 font-semibold">
-                            Depok
+                    <div className="flex flex-col items-center gap-30">
+                        <h1 className="text-base-100 font-bold text-6xl my-8 mx-2">
+                            {data.hasOwnProperty('current') ? (data as { current: { temp_c: string } }).current.temp_c : null}
+                            <a className="absolute text-3xl">
+                                °C
+                            </a>
                         </h1>
-                        <h1 className="text-base-100 font-md">
-                            Indonesia
+                    </div>
+                    <div className="flex flex-col items-center h-full gap-0">
+                        <>
+                            <h1 className="text-base-100 font-semibold text-lg">
+                                {data.hasOwnProperty('location') ? (data as { location: { name: string } }).location.name : null}
+                            </h1>
+                            <h1 className="text-base-100 font-thin text-sm ">
+                                {data.hasOwnProperty('location') ? (data as { location: { region: string } }).location.region : null}
+                            </h1>
+                        </>
+                        <h1 className="text-base-100 font-thin  text-[16px]  ">
+                            {data.hasOwnProperty('location') ? (data as { location: { country: string } }).location.country : null}   
                         </h1>
                     </div>
                 </div>
