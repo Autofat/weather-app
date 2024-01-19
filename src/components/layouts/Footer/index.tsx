5cf58ed60fa99d5c55187cf6946184717be6ab47
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2024-01-19 10:04:33 +0700

    Fix footer

--- src/components/layouts/Footer/index.tsx
+++ src/components/layouts/Footer/index.tsx
@@ -3,14 +3,14 @@ import lightLogo from '../../../../public/logo-putih.svg';
 
 const Footer = () => {
     return (
-        <footer className="items-center p-4 bg-neutral justify-center flex-col text-neutral-content md:absolute md:bottom-0 md:left-0 md:w-full">
+        <footer className="footer items-center p-4 bg-neutral justify-center flex-col text-neutral-content fixed bottom-0 left-0 w-full">
             <div className="flex-col justify-center items-center">
                 <aside>
-                    <div className="flex flex-col md:flex-row justify-center items-center md:items-center grid-flow-col gap-3">
+                    <div className="flex flex-col md:flex-row justify-center items-center md:items-center grid-flow-col sm:gap-3">
                         <div className="mb-2 md:mb-0">
                             <Image src={lightLogo} width={30} height={30} alt={"Autofat"} />
                         </div>
-                        <p>Copyright © 2024</p>
+                        <p className="mt-2 md:mt-0">Copyright © 2024</p>
                     </div>
                 </aside>
             </div>
